859c3d4a7c658cbb1c34afe1f2b85ffa410e926f
Alader <15150662912@163.com>
2020-06-23 15:15:05 +0800

    tabbar代码提交

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,21 @@
 <template>
   <div id="app">
-		<router-view></router-view>
-		<TabBar>
-			<TabBarItem link="/home" activeStyle="yellow">
-				<img slot="icon"  src="./assets/img/tabbar/home.svg"/>
-				<img slot="active-icon"  src="./assets/img/tabbar/home_active.svg"/>
-				<div slot="text">首页</div>
-			</TabBarItem>
-			<TabBarItem link="/catgory">
-				<img slot="icon"  src="./assets/img/tabbar/catgory.svg"/>
-				<img slot="active-icon"  src="./assets/img/tabbar/catgory_active.svg"/>
-				<div slot="text">分类</div>
-			</TabBarItem>
-			<TabBarItem link="cart">
-				<img slot="icon"  src="./assets/img/tabbar/cart.svg"/>
-				<img slot="active-icon"  src="./assets/img/tabbar/cart-active.svg"/>
-				<div slot="text">购物车</div>
-			</TabBarItem>
-			<TabBarItem link="profile">
-				<img slot="icon"  src="./assets/img/tabbar/profile.svg"/>
-				<img slot="active-icon"  src="./assets/img/tabbar/proflie-active.svg"/>
-				<div slot="text">我的</div>
-			</TabBarItem>
-		</TabBar>	
-		
+		<router-view/>
+    <main-tab-bar/>
   </div>
-	
 </template>
 
 <script>
-
-import TabBar from  './components/tabbar/TabBar.vue'
-import TabBarItem from './components/tabbar/TabBarItem.vue'
+import MainTabBar from 'components/content/mainTabbar/MainTabbar.vue'
 
 export default {
   name: 'App',
   components: {
-		TabBar,
-		TabBarItem
-	
+    MainTabBar
   }
 }
 </script>
 
 <style>
-
+	@import url("assets/css/base.css");
 </style>
